7c6f1d85cb6d4bd4e4e42416aa67d34b1f9d5450
MrMikitaKZ <nickita.petikov@yandex.ru>
2025-09-17 02:13:20 +0300

    Update replykeqyboard.py

--- handlers/User/keyboard/replykeqyboard.py
+++ handlers/User/keyboard/replykeqyboard.py
@@ -13,7 +13,8 @@ def get_start_menu(lang: str) -> ReplyKeyboardMarkup:
         keyboard=[
             [KeyboardButton(text=menu_text)],
             [KeyboardButton(text=accounts_text), KeyboardButton(text=site_text, web_app=WebAppInfo(url='https://gamebreaker.ru'))]
-        ], resize_keyboard=True
+        ],
+        resize_keyboard=True
     )
 
 def get_media_start_kb(lang: str) -> ReplyKeyboardMarkup:
@@ -25,8 +26,9 @@ def get_media_start_kb(lang: str) -> ReplyKeyboardMarkup:
         menu = "📋 Меню"
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text=media_menu), KeyboardButton(text=menu)],
-        ], resize_keyboard=True
+            [KeyboardButton(text=media_menu), KeyboardButton(text=menu)]
+        ],
+        resize_keyboard=True
     )
 
 def get_user_stars_kb() -> ReplyKeyboardMarkup:
@@ -34,5 +36,13 @@ def get_user_stars_kb() -> ReplyKeyboardMarkup:
         keyboard=[
             [KeyboardButton(text='1'), KeyboardButton(text='2'), KeyboardButton(text='3'), KeyboardButton(text='4'), KeyboardButton(text='5')],
             [KeyboardButton(text='6'), KeyboardButton(text='7'), KeyboardButton(text='8'), KeyboardButton(text='9'), KeyboardButton(text='10')]
-        ], one_time_keyboard=True, resize_keyboard=True
+        ],
+        one_time_keyboard=True,
+        resize_keyboard=True
     )
+
+def start_menu() -> ReplyKeyboardMarkup:
+    return get_start_menu("ru")
+
+def user_stars_kb() -> ReplyKeyboardMarkup:
+    return get_user_stars_kb()
